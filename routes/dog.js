2ee32beae27f7d91e1f2ad3c0843c54fe428beb5
Dch2301 <hamano0730@gmail.com>
2024-12-05 01:50:00 +0900

    add cat api

--- routes/dog.js
+++ routes/dog.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://dog.ceo/dog-api/', function (error, response,
-body) {
+request('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example', function (error, response, body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
 res.json(data);
